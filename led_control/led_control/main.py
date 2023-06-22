f49cee0828342dfddbf3ecc446f2893cc5512ac4
Pierre Hugo <2152709+avoid3d@users.noreply.github.com>
2023-06-22 09:21:06 +0200

    Adjust main program

--- led_control/led_control/main.py
+++ led_control/led_control/main.py
@@ -1,71 +1,15 @@
-import collections
-import struct
-import colorsys
-import serial
-import json
-from cobs import cobs
+from led_control import ahrs
+from led_control import wled
+from led_control import timing
 from datetime import datetime
 
 
-WLED_DEVICE = "/dev/tty.usbserial-0001"
-WLED_BAUDRATE = 921600
-
-AHRS_DEVICE = "/dev/tty.usbserial-16"
-AHRS_BAUDRATE = 921600
+WLED_DEVICE = "/dev/cu.usbmodem7111101"
+AHRS_DEVICE = "/dev/cu.usbserial-0001"
 
 LED_COUNT = 120
-BRIGHTNESS = 1.0
-
-FLOAT_SIZE = 4
-DELIMETER_BYTE = b"\x00"
-
-ATTITUDE_SIZE = 3
-IMU_SAMPLE_SIZE = 9
-TIMING_SIZE = 2
-
-
-PACKET_START = b"\xC9"
-PACKET_TYPE_DATA_FRAME = b"\xDA"
-PACKET_END = b"\x36"
-
-CONVERSION = 2**8 - 1
-
-
-def build_packet(leds):
-    data = b"".join([channel.to_bytes(1, "big") for led in leds for channel in led])
-    size_bytes = len(data).to_bytes(2, "big")
-    return PACKET_START + PACKET_TYPE_DATA_FRAME + size_bytes + data + PACKET_END
-
-
-def flush_leds(leds, wled_interface, brightness=1):
-    rgb_data = [colorsys.hsv_to_rgb(h, s, v) for (h, s, v) in leds]
-    rgb_data_int = [
-        (int(r * CONVERSION), int(g * CONVERSION), int(b * CONVERSION))
-        for (r, g, b) in rgb_data
-    ]
-    data = build_packet(rgb_data_int)
-    wled_interface.write(data)
-
-
-def read_ahrs(imu_interface):
-    while True:
-        stuffed_data = imu_interface.read_until(DELIMETER_BYTE)[0:-1]
-        if len(stuffed_data) == 58:
-            break
-
-    data = cobs.decode(stuffed_data)[1:]
-    report = [
-        struct.unpack("f", data[(i * FLOAT_SIZE) : (i * FLOAT_SIZE + FLOAT_SIZE)])[0]
-        for i in range(len(data) // FLOAT_SIZE)
-    ]
-    parts = []
-    index = 0
-    parts.append(report[index:ATTITUDE_SIZE])
-    index += ATTITUDE_SIZE
-    parts.append(report[index : index + IMU_SAMPLE_SIZE])
-    index += IMU_SAMPLE_SIZE
-    parts.append(report[index : index + TIMING_SIZE])
-    return parts
+BRIGHTNESS = 0.1
+SENSITIVITY = 2
 
 
 def scale(x, input_min, input_max, output_min=0, output_max=1):
@@ -81,43 +25,29 @@ def clamp(x, lower=0, upper=1):
 
 
 def main():
-    ahrs_interface = serial.Serial(AHRS_DEVICE, baudrate=AHRS_BAUDRATE)
-    wled_interface = serial.Serial(WLED_DEVICE, baudrate=WLED_BAUDRATE)
-
-    # print(wled_interface.readline(), end="")
-    # wled_interface.write("v".encode("ascii"))
-    # print(wled_interface.readline(), end="")
-    # wled_interface.write(json.dumps({"live": True}).encode("ascii"))
-
-    counter = 0
-    start_time = datetime.now()
+    ahrs_interface = ahrs.SerialAHRS(AHRS_DEVICE)
+    wled_interface = wled.SerialWLED(WLED_DEVICE)
 
     while True:
-        counter += 1
+        attitude_sample, imu_sample, timing_sample = ahrs_interface.read()
 
-        if counter > 100:
-            now = datetime.now()
-            rate = 1000000 * (counter / (now - start_time).microseconds)
-            print(f"{rate:0.2f} Hz")
+        scaled_roll = scale(attitude_sample.roll, -90, 90, 0, LED_COUNT * SENSITIVITY)
 
-            counter = 0
-            start_time = now
+        # colours = [(i / (float(LED_COUNT)), 1, 1) for i in range(LED_COUNT)]
+        # leds = [colours[int(i + scaled_roll) % 120] for i in range(LED_COUNT)]
 
-        attitude, imu_sample, timing = read_ahrs(ahrs_interface)
+        leds = [(1, 1, 1) for i in range(LED_COUNT)]
+        leds[int(scaled_roll % 120)] = (0.4, 1, 1)
+        leds[int((scaled_roll - 1) % 120)] = (0.55, 1, 1)
+        leds[int((scaled_roll + 1) % 120)] = (0.55, 1, 1)
 
-        scaled_attitude = [scale(c, -90, 90) for c in attitude]
-        clamped_attitude = [clamp(c) for c in scaled_attitude]
+        wled_interface.flush_hsv(leds, brightness=BRIGHTNESS)
 
-        colours = [(i / float(LED_COUNT), 1, 1) for i in range(LED_COUNT)]
+        if ahrs_interface.counter.ready():
+            print(f"AHRS: {ahrs_interface.counter.sample()} Hz")
 
-        position = int(clamped_attitude[0] * LED_COUNT)
-
-        if counter % 25 == 0:
-            flush_leds(
-                [colours[(i + position) % 120] for i in range(LED_COUNT)][0:80],
-                wled_interface,
-                brightness=BRIGHTNESS,
-            )
+        if wled_interface.counter.ready():
+            print(f"WLED: {wled_interface.counter.sample()} Hz")
 
 
 if __name__ == "__main__":
